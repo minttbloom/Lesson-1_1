4e106967036e744c72a74f8f0afa930a4ecfad94
Sergey Neikovich <s_neiko@outlook.com>
2021-11-12 12:28:19 +0300

    Complete work with logic operations

---
+++ logic_operations/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x = true
+	var y = false
+
+	fmt.Println(x && y == y && x)
+	fmt.Println(!(x && !y) == !x || y == x && y || x || x)
+	fmt.Println(x || y == x && !y || !x && y == (x || y) && (!x || !y))
+	fmt.Println(x || (y && !x) == (x && y) || (x && !x))
+	fmt.Println(x && (y || !x) == (x && y) || (x && !x))
+	fmt.Println(!x && !y == !x || !y)
+	fmt.Println(x && (x || y) == x)
+	fmt.Println(x || !x == x && y == x || x == y)
+	fmt.Println(x || x == x && x == x && x == x || y == x)
+	fmt.Println(!!x == x)
+	fmt.Println(x && x == !(x && y) == !x || !y)
+
+}
