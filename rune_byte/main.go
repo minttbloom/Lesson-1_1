0ed0628a4b327216bac36666244d75a0da492f3f
Kate Bourakovskaia <mintt.bloom@icloud.com>
2021-11-15 12:36:02 +0300

    edit

--- rune_byte/main.go
+++ rune_byte/main.go
@@ -4,25 +4,50 @@ import "fmt"
 
 func main() {
 
-	var r rune
-	var r1 rune
-	var r2 rune
-	var a byte
-	var b []byte
-	var c []byte
+	var s1 string = "👻"
+	var s2 string = "🎃👻"
+	var s3 string = "👽🎃👻"
+	var st1 string = "a"
 
-	r = '😂'
-	r1 = '😂'
-	r2 = '😁'
-	a = '1'
-	b = []byte("12d")
-	c = []byte("3edkiw")
+	fmt.Println([]byte(s1))
+	fmt.Println(len(s1))
 
-	fmt.Println(r)
-	fmt.Println(r1, r2)
-	fmt.Println(r, r1, r2)
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
+	fmt.Println([]byte(s1), []byte(st1))
+	fmt.Println(len(s1) + len(st1))
+
+	fmt.Println([]byte(s2))
+	fmt.Println(len(s2))
+
+	fmt.Println([]byte(s2), []byte(st1))
+	fmt.Println(len(s2) + len(st1))
+
+	fmt.Println([]byte(s3))
+	fmt.Println(len(s3))
+
+	fmt.Println([]byte(s3), []byte(st1))
+	fmt.Println(len(s3) + len(st1))
+
+	// var s1 string = "👻"
+	// var s2 string = "🎃"
+	// var s3 string = "👽"
+	// var a1 string = "a"
+
+	// fmt.Println([]byte(s1))
+	// fmt.Println(len(s1))
+
+	// fmt.Printf("%d, %d\n", []byte(s1), []byte(a1))
+	// fmt.Printf("%d\n", len(s1)+len(a1))
+
+	// fmt.Printf("%d, %d\n", []byte(s1), []byte(s2))
+	// fmt.Printf("%d\n", len(s1)+len(s2))
+
+	// fmt.Printf("%d, %d, %d\n", []byte(s1), []byte(s2), []byte(a1))
+	// fmt.Printf("%d\n", len(s1)+len(s2)+len(a1))
+
+	// fmt.Printf("%d, %d, %d\n", []byte(s1), []byte(s2), []byte(s3))
+	// fmt.Printf("%d\n", len(s1)+len(s2)+len(s3))
+
+	// fmt.Printf("%d, %d,%d, %d\n", []byte(s1), []byte(s2), []byte(s3), []byte(a1))
+	// fmt.Printf("%d\n", len(s1)+len(s2)+len(s3)+len(a1))
 
 }
