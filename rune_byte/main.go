568c3b5a009ff2227d4a9096993724969b5bbb59
Kate Bourakovskaia <mintt.bloom@icloud.com>
2021-11-17 13:27:09 +0300

    edit

--- rune_byte/main.go
+++ rune_byte/main.go
@@ -4,16 +4,16 @@ import "fmt"
 
 func main() {
 
-	// var s1 string = "👻"
+	// var s4 string = "👻"
 	// var s2 string = "🎃👻"
 	// var s3 string = "👽🎃👻"
 	// var st1 string = "a"
 
-	// fmt.Println([]byte(s1))
-	// fmt.Println(len(s1))
+	// fmt.Println([]byte(s4))
+	// fmt.Println(len(s4))
 
-	// fmt.Println([]byte(s1), []byte(st1))
-	// fmt.Println(len(s1) + len(st1))
+	// fmt.Println([]byte(s4), []byte(st1))
+	// fmt.Println(len(s4) + len(st1))
 
 	// fmt.Println([]byte(s2))
 	// fmt.Println(len(s2))
@@ -27,27 +27,41 @@ func main() {
 	// fmt.Println([]byte(s3), []byte(st1))
 	// fmt.Println(len(s3) + len(st1))
 
-	var s1 string = "👻"
-	var s2 string = "🎃"
-	var s3 string = "👽"
-	var a1 string = "a"
+	var s1 string = "a"  //1
+	var s2 string = "я"  //2
+	var s3 string = "字"  //3
+	var s4 string = "👻"  //4
+	var s6 string = "❄️" //6
+	var s7 string = "🏳️" //7
 
-	fmt.Println([]byte(s1))
-	fmt.Println(len(s1))
+	fmt.Println([]byte(s4))
+	fmt.Println(len(s4)) //4
 
-	fmt.Printf("%d, %d\n", []byte(s1), []byte(a1))
-	fmt.Printf("%d\n", len(s1)+len(a1))
+	fmt.Printf("%d, %d\n", []byte(s2), []byte(s2))
+	fmt.Printf("%d\n", len(s2)+len(s2)) //4
 
-	fmt.Printf("%d, %d\n", []byte(s1), []byte(s2))
-	fmt.Printf("%d\n", len(s1)+len(s2))
+	fmt.Printf("%d, %d\n", []byte(s4), []byte(s1))
+	fmt.Printf("%d\n", len(s4)+len(s1)) //5
 
-	fmt.Printf("%d, %d, %d\n", []byte(s1), []byte(s2), []byte(a1))
-	fmt.Printf("%d\n", len(s1)+len(s2)+len(a1))
+	fmt.Printf("%d, %d\n", []byte(s4), []byte(s2))
+	fmt.Printf("%d\n", len(s4)+len(s2)) //6
 
-	fmt.Printf("%d, %d, %d\n", []byte(s1), []byte(s2), []byte(s3))
-	fmt.Printf("%d\n", len(s1)+len(s2)+len(s3))
+	fmt.Printf("%d, %d\n", []byte(s6), []byte(s1))
+	fmt.Printf("%d\n", len(s6)+len(s1)) //7
 
-	fmt.Printf("%d, %d,%d, %d\n", []byte(s1), []byte(s2), []byte(s3), []byte(a1))
-	fmt.Printf("%d\n", len(s1)+len(s2)+len(s3)+len(a1))
+	fmt.Printf("%d, %d\n", []byte(s6), []byte(s2))
+	fmt.Printf("%d\n", len(s6)+len(s2)) //8
+
+	fmt.Printf("%d, %d\n", []byte(s6), []byte(s3))
+	fmt.Printf("%d\n", len(s6)+len(s3)) //9
+
+	fmt.Printf("%d, %d\n", []byte(s6), []byte(s4))
+	fmt.Printf("%d\n", len(s6)+len(s4)) //10
+
+	fmt.Printf("%d, %d\n", []byte(s7), []byte(s4))
+	fmt.Printf("%d\n", len(s7)+len(s4)) //11
+
+	fmt.Printf("%d, %d, %d\n", []byte(s7), []byte(s3), []byte(s2))
+	fmt.Printf("%d\n", len(s7)+len(s3)+len(s2)) //12
 
 }
